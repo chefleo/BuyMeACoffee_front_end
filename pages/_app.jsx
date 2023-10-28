45b2223acccd6f3ef4e1ba14343a6f3e839e3c38
chefleo <simon.digiorgio@gmail.com>
2023-10-28 12:40:33 +0200

    Minor change and install sharp

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,39 +1,48 @@
-import '../styles/globals.css'
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
+import "../styles/globals.css";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
 
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { publicProvider } from 'wagmi/providers/public'
+import { publicProvider } from "wagmi/providers/public";
 import { infuraProvider } from "wagmi/providers/infura";
-import { baseGoerli } from 'wagmi/chains'
+import { baseGoerli } from "wagmi/chains";
 
 if (!process.env.INFURA_ID) {
-  throw new Error('You need to provide WALLET_CONNECT_PROJECT_ID env variable')
+  throw new Error("You need to provide INFURA_ID env variable");
 }
 
-const chains = [baseGoerli]
-const projectId = process.env.INFURA_ID
+const chains = [baseGoerli];
+const projectId = process.env.INFURA_ID;
 
-const { publicClient } = configureChains(chains, [infuraProvider({ projectId }), publicProvider()])
+const { publicClient } = configureChains(chains, [
+  infuraProvider({ projectId }),
+  publicProvider(),
+]);
 const wagmiConfig = createConfig({
-  connectors: [new CoinbaseWalletConnector({
-    chains,
-    options: {
-      appName: "BuyMeACoffee",
-    },
-  })],
-  publicClient
-})
+  connectors: [
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "BuyMeACoffee",
+      },
+    }),
+  ],
+  publicClient,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-  <>
-    <WagmiConfig config={wagmiConfig}>
-      <Component {...pageProps} />
-    </WagmiConfig>
-  </>
-)
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
